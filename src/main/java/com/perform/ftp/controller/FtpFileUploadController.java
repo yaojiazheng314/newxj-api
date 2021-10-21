5f78b129613d08491a64b9c2ccf962639c4b7fd7
yaojiazheng314 <1179892613@qq.com>
2021-10-21 11:43:00 +0800

    Initial commit

---
+++ src/main/java/com/perform/ftp/controller/FtpFileUploadController.java
@@ -0,0 +1,76 @@
+package com.perform.ftp.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.perform.base.ApiController;
+import com.perform.config.IConstant;
+import com.perform.ftp.util.FtpFileUtil;
+import com.perform.pojo.FeedBackReport;
+import com.perform.utils.JSONResult;
+import com.perform.v2.pojo.FeedBackReportDepartmentReason;
+import com.perform.v2.service.FeedBackReportV2Service;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+
+@Controller
+
+public class FtpFileUploadController extends ApiController {
+
+    //ftp处理文件上传
+    @PostMapping(value = "/feedBack/uploadPic")
+    public @ResponseBody
+    JSONResult upload(@RequestParam List<MultipartFile> files
+                     ) throws IOException {
+
+        //FTP 基础服务地址
+        String baseUrl = IConstant.FTPURL;
+        List<String> urlPicList=new ArrayList<>();
+        for (MultipartFile file : files) {
+            String filePath ="";
+            String fileName = file.getOriginalFilename();
+            InputStream inputStream = file.getInputStream();
+             filePath += baseUrl;
+
+
+            Boolean flag = FtpFileUtil.uploadFile(fileName, inputStream);
+            if (flag == true) {
+                System.out.println("ftp上传成功！");
+                filePath += fileName;
+            }
+
+            urlPicList.add(filePath);
+        }
+        JSONObject data = new JSONObject();
+        data.put("urlPicList", urlPicList);
+        return JSONResult.ok(data);
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/perform/ftp/util/FtpFileUtil.java
@@ -0,0 +1,52 @@
+package com.perform.ftp.util;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FtpFileUtil {
+    //ftp服务器ip地址
+    private static final String FTP_ADDRESS = "192.168.10.105";
+    //端口号
+    private static final int FTP_PORT = 21;
+    //用户名
+    private static final String FTP_USERNAME = "dell";
+    //密码
+    private static final String FTP_PASSWORD = "yao123";
+    //图片路径
+    private static final String FTP_BASEPATH = "/localuser/newjx-ftp/images";
+
+    public  static boolean uploadFile(String originFileName,InputStream input){
+        boolean success = false;
+        FTPClient ftp = new FTPClient();
+        ftp.setControlEncoding("GBK");
+        try {
+            int reply;
+            ftp.connect(FTP_ADDRESS, FTP_PORT);// 连接FTP服务器
+            ftp.login(FTP_USERNAME, FTP_PASSWORD);// 登录
+            reply = ftp.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+                return success;
+            }
+            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+            ftp.makeDirectory(FTP_BASEPATH );
+            ftp.changeWorkingDirectory(FTP_BASEPATH );
+            ftp.storeFile(originFileName,input);
+            input.close();
+            ftp.logout();
+            success = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch (IOException ioe) {
+                }
+            }
+        }
+        return success;
+    }
+}
